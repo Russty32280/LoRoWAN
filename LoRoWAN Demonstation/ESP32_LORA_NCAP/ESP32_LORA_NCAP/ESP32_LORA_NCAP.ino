8e36f92260c1146d4ff72096289c33e0ba164f7e
Russty32280 <traffo17@students.rowan.edu>
2017-12-20 18:12:44 -0500

    JSON sort of working. Issues with FNID and other fields

--- LoRoWAN Demonstation/ESP32_LORA_NCAP/ESP32_LORA_NCAP/ESP32_LORA_NCAP.ino
+++ LoRoWAN Demonstation/ESP32_LORA_NCAP/ESP32_LORA_NCAP/ESP32_LORA_NCAP.ino
@@ -16,8 +16,6 @@
 
 
 RH_RF95 driver(16, 17); // Generate a RadioHead Object using pins 16 and 17 for reset and CS
-
-// Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
 
@@ -31,13 +29,12 @@ AWS_IOT hornbill;   // AWS_IOT instance
 char WIFI_SSID[]="Rowan_IOT";
 char WIFI_PASSWORD[]="";
 char HOST_ADDRESS[]="a213qaaw8pshxz.iot.us-east-1.amazonaws.com";
-char CLIENT_ID[]= "LoRoWANESP32";
+char CLIENT_ID[]= "LoRoWAN2";
 char TOPIC_NAME[]= "NetworkNode1/General";
 
 int status = WL_IDLE_STATUS;
 int tick=0,msgCount=0,msgReceived = 0;
-char payload[512];
-char rcvdPayload[512];
+char payload[251];
 
 
 
@@ -86,14 +83,15 @@ void setup() {
     driver.setFrequency(915.0);
     manager.setTimeout(1000);
     Serial.println("Passed Init");
-
+    
     /*************************
     * Example JSON to AWS
     **************************/
 
-    StaticJsonBuffer<200> jsonBuffer;
-
+    
+    /*
     JsonObject& root = jsonBuffer.createObject();
+    root["FNID"] = "7211";
     root["NNID"] = "1";
     root["TNID"] = "1";
     root["ChanID"] = "1";
@@ -113,95 +111,137 @@ void setup() {
         }
            
 
-    
+    */
 
 }
 
-uint8_t data[] = "9";
-uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+char data[512];
+uint8_t buf[100];
 
 bool IsSending = true;
 
+uint16_t FNID;
+uint16_t NNID;
+uint16_t TNID;
+uint16_t ChanID;
+
+
+uint8_t len = sizeof(buf);
+uint8_t from;
+
+//JsonObject& root = jsonBuffer.createObject();
+
 void loop() {
-    uint8_t len = sizeof(buf);
-    uint8_t from;
+    Serial.println("Starting Loop");
+
+    StaticJsonBuffer<200> jsonBuffer;
+    StaticJsonBuffer<200> jsonBuffer2;
+    StaticJsonBuffer<200> jsonBuffer3;
+
+
     if (IsSending)
     {
-      data[0] = 0x00;
-      data[1] = 0x01; 
+      
+      Serial.println("Json Object");
+      JsonObject& root = jsonBuffer.createObject();
+      Serial.println("Created JSOn Object for Root1");
+      root["FNID"] = 7211;
+      root["NNID"] = 1;
+      root["TNID"] = 1;
+      root["ChanID"] = 1;
+      root["Data"] = 1;
+      root["temp"] = 85; 
+
+      Serial.println("Before PrintTo Root1");
+      root.printTo(data, sizeof(data));
+      Serial.println("After PrintTo Root1");
+      /*
+      FNID = 7211;
+      TNID = 1;
+      NNID = 1;
+      ChanID = 1;
+
+      data[0] = (uint8_t)(FNID&0x00FF);   // Set first byte of data to be sent as the FuncID High Byte  
+      data[1] = (uint8_t)((FNID&0xFF00)>>8);   // Set Second byte to FuncID Low Byte
+      data[2] = (uint8_t)(NNID&0x00FF);;   // Next, the Network Node ID which requested the data
+      data[3] = (uint8_t)((NNID&0xFF00)>>8);
+      data[4] = (uint8_t)(TNID&0x00FF);  // Transducer Node High Byte
+      data[5] = (uint8_t)((TNID&0xFF00)>>8);  // Transducer Node Low Byte
+      data[6] = (uint8_t)(ChanID&0x00FF);  // Channel ID High
+      data[7] = (uint8_t)((ChanID&0xFF00)>>8);   //  Channel ID Low
+      */
+
+      
+      Serial.println(data);
       Serial.println("Sending Data");
-      if (!manager.sendtoWait(data, sizeof(data), 1))
+      //uint8_t intData = (uint8_t)data;
+      if (!manager.sendtoWait((uint8_t*)data, sizeof(data), 1))
       {
         Serial.println("sendtoWait failed");
         sprintf(payload, "sendtoWait failed");
       }
       else
+        Serial.println("Else, change to recieving");
         IsSending = false;
     }
   else
   {
-      if (manager.recvfromAck(buf, &len, &from))
-      {
-        Serial.println("Got something");
-        Serial.println(from);
-        for (int i = 0; i<len; i++)
-        {
-          Serial.println(buf[i]);
-          payload[i] = (char)(buf[i]%30)+'0';
-        }
-      }
-      IsSending = true;
-  }
-  //if (manager.available())
-  //{
-    // Wait for a message addressed to us from the client
-    //uint8_t len = sizeof(buf);
-    //uint8_t from;
-    /*
-    if (manager.recvfromAck(buf, &len, &from))
-    {
-      if (from == REGISTRATION_ADDRESS)
-      {
-        Serial.println("New device acknowledged. Requesting to change address to 9");
-        sprintf(payload, "New Device Acknowledged");
-      }
-      else if (from == 9)
-      {
-        Serial.println("Device acknowleged at address 9.");
-        sprintf(payload, "Device acknowledged at address 9");
-      }
-      else
-      {
-        Serial.print("got request from : 0x");
-        Serial.print(from, HEX);
-        Serial.print(": ");
-        Serial.println((char*)buf);
-        Serial.print("RSSI: ");
-        Serial.println(driver.lastRssi(), DEC);
-        sprintf(payload, "Got a request from some other device");
-      }
-      // Send a reply back to the originator client
-      */
-      //data[0] = 0x00;
-      //data[1] = 0x01; 
-      //Serial.println("Sending Data");
-      //if (!manager.sendtoWait(data, sizeof(data), from))
-      //{
-      //  Serial.println("sendtoWait failed");
-      //  sprintf(payload, "sendtoWait failed");
-      //}
+      Serial.println("Receving Mode");
 
-     
+      while (!(manager.recvfromAck(buf, &len, &from)))
+      {
+        //Serial.println("Waiting");
+      }
+      Serial.println("Got Something");
+      IsSending = true;
+      Serial.println("Changed IsSending");
+
+     //buf is huge!!
+     //This reads important stuff from buff
+     //And repackages into smaller JSON
+    
+    JsonObject& root2 = jsonBuffer2.parseObject((char*)buf);
+    Serial.println("Generated Root2");
+    uint16_t FunctionID = root2["FNID"];
+    uint16_t NNID = root2["NNID"];
+    uint16_t TNID = root2["TNID"];
+    uint8_t ChanID = root2["ChanID"];
+    uint32_t Data = root2["Data"];
+
+    /*
+    JsonObject& root3 = jsonBuffer.createObject();
+    Serial.println("Created Root3")
+    root3["FNID"] = buf[0]<<8 + buf[1];
+    root3["NNID"] = buf[2]<<8 + buf[3];
+    root3["TNID"] = buf[4]<<8 + buf[5];
+    root3["ChanID"] = buf[6]<<8 + buf[7];
+    root3["Data"] = buf[8]<<8 + buf[9];
+    root3["temp"] = "85";
+
+    root3.printTo(payload, sizeof(payload));
+    Serial.println(payload);
+    */
+    
+    JsonObject& root3 = jsonBuffer3.createObject();
+    Serial.println("Generated Root3");
+    root3["FNID"] = FunctionID;
+    root3["NNID"] = NNID;
+    root3["TNID"] = TNID;
+    root3["ChanID"] = ChanID;
+    root3["Data"] = Data;
+    root3["temp"] = "85";
+  
+    root3.printTo(payload, sizeof(payload));
+       
      if(hornbill.publish(TOPIC_NAME,payload) == 0)   // Publish the message(Temp and humidity)
         {        
             Serial.print("Publish Message:");   
-            Serial.println(payload);
+            //Serial.println(buf);
         }
         else
         {
             Serial.println("Publish failed");
         }
-       
-    //}
-    delay(1000);
   }
+    delay(1000);
+}
--- LoRoWAN Demonstation/UNO_LORA_CLIENT/UNO_LORA_CLIENT.ino
+++ LoRoWAN Demonstation/UNO_LORA_CLIENT/UNO_LORA_CLIENT.ino
@@ -1,131 +1,151 @@
-// rf95_reliable_datagram_client.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging client
-// with the RHReliableDatagram class, using the RH_RF95 driver to control a RF95 radio.
-// It is designed to work with the other example rf95_reliable_datagram_server
-// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W 
-
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
 #include <SPI.h>
+#include <ArduinoJson.h>
 
 //#define CLIENT_ADDRESS 1
 uint8_t CLIENT_ADDRESS = 1;
 
 #define SERVER_ADDRESS 2
 
-// Singleton instance of the radio driver
-//RH_RF95 driver;
-RH_RF95 driver(4, 3); // Rocket Scream Mini Ultra Pro with the RFM95W
+RH_RF95 driver(4, 3); 
 
-// Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
-
-// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
-//#define Serial SerialUSB
 
 void setup() 
 {
-  // Rocket Scream Mini Ultra Pro with the RFM95W only:
-  // Ensure serial flash is not interfering with radio communication on SPI bus
-//  pinMode(4, OUTPUT);
-//  digitalWrite(4, HIGH);
-
-
-  
+ 
   Serial.begin(9600);
   while (!Serial) ; // Wait for serial port to be available
   if (!manager.init())
     Serial.println("init failed");
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
   driver.setTxPower(23, false);
-  // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
-  // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
-  // Failure to do that will result in extremely low transmit powers.
-  //  driver.setTxPower(14, true);
 
 
   driver.setModemConfig(RH_RF95::ModemConfigChoice::Bw500Cr45Sf128);
   driver.setFrequency(915.0);
   manager.setTimeout(2000);
+  Serial.println("Setup Complete");
 
 }
 
 // Dont put this on the stack:
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-uint8_t isFirstRun = 1;
-uint16_t temp;
-uint8_t data[8];
-uint16_t SensorData = 0;  // Fake Sensor Data
-uint8_t TNID0 = 0x00; // Transducer Node ID Low Byte
-uint8_t TNID1 = 0x01; // Transducer Node ID High Byte
+char payload[64];
 
+
+uint16_t Channel2Read();
+uint16_t Channel1Read();
+
+uint16_t ReadSDSCST(uint8_t ChanID);
+
+//uint16_t FuncID;
+//uint16_t NNID;
+//uint16_t TNID = 0x0001;
+//uint16_t ChanID;
+uint16_t SensorData;
+
+uint8_t len = sizeof(buf);
+uint8_t from;
 
 void loop()
 {
-  uint8_t len = sizeof(buf);
-  uint8_t from;  
+  //Serial.println("In the loop");
+  StaticJsonBuffer<200> jsonBuffer;
+  StaticJsonBuffer<200> jsonBuffer2;
   if (manager.recvfromAck(buf, &len, &from))
   {
+    Serial.println("Received Something");
     for(int i = 0; i<len; i++)
-      Serial.println(buf[i]);
-    if ((buf[1] == 0x01) && (buf[0] == 0x00)) 
+      Serial.print((char)buf[i]);
+    
+    JsonObject& root = jsonBuffer.parseObject(buf);
+    uint16_t FNID = root["FNID"];
+    Serial.println(FNID);
+    uint16_t NNID = root["NNID"];
+    uint16_t TNID = root["TNID"];
+    uint16_t ChanID = root["ChanID"];
+    uint16_t Data = root["Data"];
+    /*
+    FuncID = buf[0] + buf[1]<<8; 
+    NNID = buf[2] + buf[3]<<8;
+    TNID = buf[4] + buf[5]<<8;
+    ChanID = buf[6] + buf[7]<<8;
+    */
+    
+    switch(FNID)
     {
-      SensorData++;
-      data[0] = 0x00;   // Set first byte of data to be sent as the FuncID High Byte  
-      data[1] = 0x01;   // Set Second byte to FuncID Low Byte
-      data[2] = from;   // Next, the Network Node ID which requested the data
-      data[3] = TNID0;  // Transducer Node High Byte
-      data[4] = TNID1;  // Transducer Node Low Byte
-      data[5] = buf[5];  // Channel ID High
-      data[6] = buf[6];   //  Channel ID Low
-      data[7] = SensorData; // Just the incremented value
-      Serial.println("Sending to rf95_reliable_datagram_server");
-      if (!manager.sendtoWait(data, sizeof(data), from))
+      case 7211:
+        Serial.println("received 7211");
+        SensorData = ReadSDSCST(ChanID);
+        break;
+      /*
+      case "7217":
+        Payload = WriteSDSCST(ChanID, Data);
+        break;
+      */
+    }
+
+    Serial.println(SensorData);
+    JsonObject& root2 = jsonBuffer2.createObject();
+    SensorData = (uint16_t)random(73, 88);
+    root2["Data"] = SensorData;
+    root2["NNID"] = NNID;
+    root2["TNID"] = TNID;
+    root2["ChanID"] = ChanID;
+    root2["FNID"] = FNID;
+    root2.printTo(payload, sizeof(payload));
+    
+      /*
+      payload[0] = (uint8_t)(FuncID&0x00FF);   // Set first byte of data to be sent as the FuncID High Byte  
+      payload[1] = (uint8_t)((FuncID&0xFF00)>>8);   // Set Second byte to FuncID Low Byte
+      payload[2] = (uint8_t)(NNID&0x00FF);;   // Next, the Network Node ID which requested the data
+      payload[3] = (uint8_t)((NNID&0xFF00)>>8);
+      payload[4] = (uint8_t)(TNID&0x00FF);  // Transducer Node High Byte
+      payload[5] = (uint8_t)((TNID&0xFF00)>>8);  // Transducer Node Low Byte
+      payload[6] = (uint8_t)(ChanID&0x00FF);  // Channel ID High
+      payload[7] = (uint8_t)((ChanID&0xFF00)>>8);   //  Channel ID Low
+      payload[8] = (uint8_t)(SensorData&0x00FF); // Just the incremented value
+      payload[9] = (uint8_t)((SensorData&0xFF00)>>8);
+      */
+
+
+    //for(int L=0; L<10; L++)
+      //Serial.print(payload[L]);
+    
+    if (!manager.sendtoWait(payload, sizeof(payload), from))
       {
         Serial.println("Send to wait failed");
       }
     }
-  }
 
 
-
-  //data[0] = 'L';
-  //temp = analogRead(0);
-  //if(temp > 255) temp = 255;
-  //data[1] = (uint8_t) temp;
-  //Serial.print("Data:");
-  //Serial.println(data[1]);
-  
-  
-    
-  // Send a message to manager_server
-  //if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
-  //{
-    
-    
-    // Now wait for a reply from the server
-  //  uint8_t len = sizeof(buf);
-  //  uint8_t from;   
-  //  if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
-  //  {
-  //    Serial.print("got reply from : 0x");
-  //    Serial.print(from, HEX);
-  //    Serial.print(": ");
-  //    Serial.println((char*)buf); 
-  //  }
-  //  else
-  //  {
-  //    Serial.println("No reply, is rf95_reliable_datagram_server running?");
-  //  }
-  //}
-  //else
-  //  Serial.println("sendtoWait failed");
   delay(50);
+}
+
+
+uint16_t ReadSDSCST(uint16_t ChanID)
+{
+  switch(ChanID)
+  {
+    case 1:
+      return Channel1Read();
+      break;
+    case 2:
+      return Channel2Read();
+      break;
+  }
+}
+
+
+
+uint16_t Channel1Read()
+{
+  return (uint16_t)random(73, 83);  // Simulate a random temperature and return value
+}
+
+uint16_t Channel2Read()
+{
+  return (uint16_t)random(78, 88);  // Simulate a random temperature and return value
 }
 
